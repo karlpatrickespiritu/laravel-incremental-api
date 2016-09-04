8d2744b9d4a075e555519762cb9abe3e854f5bf3
karlpatrickespiritu <wiwa.espiritu@gmail.com>
2016-09-04 15:42:13 +0800

    migrations, seeders, routes, api transformer

---
+++ app/Http/Controllers/LessonsController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Lesson;
+use App\Transformers\LessonTransformer;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+
+class LessonsController extends Controller
+{
+
+    protected $lessonTransformer;
+
+    public function __construct(LessonTransformer $lessonTransformer) 
+    {
+        $this->lessonTransformer = $lessonTransformer;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $lessons = Lesson::all();
+        return $this->lessonTransformer->transformCollection($lessons->toArray());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $lesson = Lesson::find($id);
+        return $this->lessonTransformer->transform($lesson);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Transformers/LessonTransformer.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Transformers;
+
+class LessonTransformer extends Transformer
+{
+	public function transform($lesson)
+	{
+		return [
+			'title' => $lesson['title'],
+			'body' => $lesson['body'],
+		];
+	}
+}
